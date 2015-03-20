c293641818a18cd737221c509c27006f040bfc3a
Heather Piwowar <hpiwowar@gmail.com>
2015-03-20 10:26:00 -0700

    try another way to set redis configs

--- rq_dashboard/app.py
+++ rq_dashboard/app.py
@@ -8,4 +8,7 @@ app = Flask(__name__)
 if os.getenv('RQ_DASHBOARD_SETTINGS'):
     app.config.from_envvar('RQ_DASHBOARD_SETTINGS')
 
+app.config['REDIS_DB'] = 3
+app.config['REDIS_URL'] = os.getenv("REDIS_URL")
+
 RQDashboard(app, '')
